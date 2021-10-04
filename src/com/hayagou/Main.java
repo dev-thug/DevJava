3170445c9710b911a370552ffe6fb90458c5aa3c
hayagou <de0978@gmail.com>
2021-10-04 15:35:27 +0900

    add 프랜즈 4 블럭

--- src/com/hayagou/Main.java
+++ src/com/hayagou/Main.java
@@ -1,34 +1,12 @@
 package com.hayagou;
 
-import java.util.Scanner;
-import java.util.Stack;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>();
-
-        // 스택에 데이터를 저장
-        stack.push("첫번째 데이터");
-        stack.push("두번째 데이터");
-        stack.push("세번째 데이터");
-
-        // 세번째 데이터가 출력됨
-        System.out.println(stack.peek());
-
-        // 가장 마지막에 들어온 데이터 반환후 제거
-        String data = stack.pop();
-
-        stack.push("네번째 데이터");
-        // 네번째 데이터가 출력됨
-        System.out.println(stack.peek());
-
-        System.out.println();
-        // 두번째 데이터 저장
-        stack.push(data);
-
-        // 스택이 없을때 까지 반복
-        while(!stack.isEmpty()){
-            System.out.println("모든데이터 출력 : "+stack.pop());
-        }
     }
 }
--- src/com/hayagou/kakao/Friends4Block.java
+++ src/com/hayagou/kakao/Friends4Block.java
@@ -1,55 +1,73 @@
 package com.hayagou.kakao;
 
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Friends4Block {
     public static int solution(int m, int n, String[] board) {
         int answer = 0;
-        String[][] boardArray = new String[m][n];
+        int removed = 0;
+        boolean flag = true;
+        String pattern = "^[A-Z]*$";
+        String[][] array = new String[m][n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             String[] temp = board[i].split("");
-            for (int j = 0; j < m; j++) {
-                boardArray[i][j] = temp[j];
+            for (int j = 0; j < n; j++) {
+                array[i][j] = temp[j];
             }
         }
-// 출력
-//        for (int i = 0; i < n; i++) {
-//
-//            for (int j = 0; j < m; j++) {
-//                System.out.print(boardArray[i][j]);
-//            }
-//            System.out.println();
-//        }
+        List<String>[] list = new List[n];
 
-        String[][] mask = new String[4][4];
+        for (int j = 0; j < n; j++) {
+            list[j] = new LinkedList<>();
+            for (int i = 0; i < m; i++) {
+                list[j].add(0, array[i][j]);
+            }
+        }
 
-        for (int i = 0; i < n - 1; i++) {
 
-            for (int j = 0; j < m - 1; j++) {
-                mask[i][j] = boardArray[i][j];
-                mask[i][j + 1] = boardArray[i][j + 1];
-                mask[i + 1][j] = boardArray[i + 1][j];
-                mask[i + 1][j + 1] = boardArray[i + 1][j + 1];
-                if (mask[i][j].equalsIgnoreCase(mask[i][j + 1]) &&
-                        mask[i][j].equalsIgnoreCase(mask[i][j + 1]) &&
-                        mask[i][j].equalsIgnoreCase(mask[i + 1][j]) &&
-                        mask[i][j].equalsIgnoreCase(mask[i + 1][j + 1])
-                ){
-                    mask[i][j] = boardArray[i][j];
-                    mask[i][j + 1] = boardArray[i][j + 1];
-                    mask[i + 1][j] = boardArray[i + 1][j];
-                    mask[i + 1][j + 1] = boardArray[i + 1][j + 1];
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < n - 1; i++) {
+
+                int len = Math.min(list[i].size(), list[i + 1].size());
+                for (int j = 0; j < len - 1; j++) {
+                    String a = list[i].get(j);
+                    String b = list[i].get(j + 1);
+                    String c = list[i + 1].get(j);
+                    String d = list[i + 1].get(j + 1);
+
+                    if (a.equalsIgnoreCase(b) && a.equalsIgnoreCase(c) && a.equalsIgnoreCase(d)) {
+                        list[i].set(j, a.toLowerCase());
+                        list[i].set(j + 1, a.toLowerCase());
+                        list[i + 1].set(j, a.toLowerCase());
+                        list[i + 1].set(j + 1, a.toLowerCase());
+
+                        flag = true;
+                    }
                 }
             }
+
+            for (int i = 0; i < n; i++) {
+                list[i] = new LinkedList<>(list[i].stream().filter(s -> Pattern.matches(pattern, s)).collect(Collectors.toList()));
+            }
         }
 
 
+        for (int i = 0; i < n; i++) {
+            removed += list[i].size();
+        }
+
+        answer = (m * n) - removed;
         return answer;
     }
 
     public static void main(String[] args) {
-        int m = 6;
-        int n = 6;
-        String[] board = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
+        int m = 4;
+        int n = 5;
+        String[] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
         System.out.println(solution(m, n, board));
     }
 }
